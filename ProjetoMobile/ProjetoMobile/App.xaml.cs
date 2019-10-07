d3fed3540c336c38a0b26c6922458ad1fe2a6c1b
Ezequiel Pereira da Silva <kiel.btu@hotmail.com>
2019-10-07 17:24:36 -0300

    Parou de funcionar

--- ProjetoMobile/ProjetoMobile/App.xaml.cs
+++ ProjetoMobile/ProjetoMobile/App.xaml.cs
@@ -16,8 +16,8 @@ namespace ProjetoMobile
             var Banco = Pasta.CreateFile("BancoX.db", PCLExt.FileStorage.CreationCollisionOption.OpenIfExists);
             Conexao = new SQLite.SQLiteConnection(Banco.Path);
 
-            Conexao.Execute("CREATE TABLE IF NOT EXISTS Login (id INTEGER PRIMARY KEY AUTOINCREMENT NOTNULL UNIQUE" +
-                ", nome TEXT NOTNULL, usuario TEXT NOTNULL UNIQUE, email TEXT NOTNULL UNIQUE, senha TEXT NOTNULL, cep NOTNULL)");
+            Conexao.Execute("CREATE TABLE IF NOT EXISTS Login (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE" +
+                ", nome TEXT NOT NULL, usuario TEXT NOT NULL UNIQUE, email TEXT NOT NULL UNIQUE, senha TEXT NOT NULL, cep NOT NULL)");
 
             InitializeComponent();
 
